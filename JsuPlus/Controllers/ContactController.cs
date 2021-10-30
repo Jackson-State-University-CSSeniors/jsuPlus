8525472ee47274076724b3aef15bafaa900d4253
daspalo <daspalo@protonmail.com>
2021-10-30 00:12:45 -0500

    Add project files.

---
+++ JsuPlus/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace JsuPlus.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        //GET: /Contact
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        //GET: /ContactEmail
+        public string Welcome()
+        {
+            return "Please email us at support@gmail.com";
+        }
+    }
+}

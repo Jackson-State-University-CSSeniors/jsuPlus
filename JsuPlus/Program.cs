38940fe481647b52fe766bbc68293d48960e8b10
daspalo <daspalo@protonmail.com>
2021-11-02 23:09:41 -0500

    Connecting database

--- JsuPlus/Program.cs
+++ JsuPlus/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace JsuPlus
 {
@@ -13,6 +14,17 @@ namespace JsuPlus
     {
         public static void Main(string[] args)
         {
+            //string connectionstring = @"server=us-cdbr-east-04.cleardb.com;userid=b06d5d4e606035;password=8bdddad9;database=heroku_30ebef2574eacbe";
+            //MySqlConnection connection = null;
+
+            ////try
+            ////{
+            ////    connection= new MySqlConnection(connectionstring);
+            ////    connection.Open();
+
+                
+            ////}
+
             CreateHostBuilder(args).Build().Run();
         }
 
